340a1f93cb86af942534598451101716d55efed2
Devin Phillips <144kfilms@gmail.com>
2026-09-12 23:12:56 -0500

    Install Istio service mesh: ingress gateway, sidecar injection, STRICT mTLS for app pods. Required resource right-sizing (istiod, gateway, app, sidecar) to fit within free-tier-eligible t3.micro capacity.

--- terraform-eks/variables.tf
+++ terraform-eks/variables.tf
@@ -42,15 +42,15 @@ variable "node_instance_types" {
 
 variable "node_desired_size" {
   type    = number
-  default = 2
+  default = 5
 }
 
 variable "node_min_size" {
   type    = number
-  default = 2
+  default = 4
 }
 
 variable "node_max_size" {
   type    = number
-  default = 3
+  default = 5
 }
