f6c33571ab47a90e1c883d48b2c09944ed993c89
Devin Phillips <144kfilms@gmail.com>
2026-09-15 18:32:36 -0500

    Move terraform-eks state to versioned S3 backend with native locking

---
+++ terraform-eks/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "vulntrack-tfstate-825990809758"
+    key          = "eks/terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
